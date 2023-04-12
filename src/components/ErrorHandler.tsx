d4bda2d6694b54a0933a77d649ec03e4affb02fb
omar.ashraf <omar.ashraf@hmaserv.com>
2023-04-12 07:42:34 +0200

    Add reload functionality

--- src/components/ErrorHandler.tsx
+++ src/components/ErrorHandler.tsx
@@ -61,8 +61,60 @@ export default function ErrorHandler({
   return (
     <div className="w-fit">
       <>
-        {status && <span>Correct! your word was {word}</span>}
-        {attempts === 6 && !status && <span>Wrong. Your word was {word}</span>}
+        {status && (
+          <div className="flex flex-col gap-y-2">
+            <button
+              onClick={() => {
+                window.location.reload();
+              }}
+              className="flex p-2 mx-auto text-white align-middle bg-blue-400 gap-x-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                />
+              </svg>
+              Restart
+            </button>
+            <span>Correct! your word was {word}</span>
+          </div>
+        )}
+        {attempts === 6 && !status && (
+          <div className="flex flex-col gap-y-2">
+            <button
+              onClick={() => {
+                window.location.reload();
+              }}
+              className="flex p-2 mx-auto text-white align-middle bg-blue-400 gap-x-2"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                />
+              </svg>
+              Restart
+            </button>
+            <span>Wrong. your word was {word}</span>
+          </div>
+        )}
       </>
     </div>
   );
